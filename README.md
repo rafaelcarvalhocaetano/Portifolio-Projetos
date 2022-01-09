ec31233f1911a4683719b3577b7fcb90c9db9740
Rafael Carvalho <rapha.pse@outlook.com>
2022-01-09 20:36:47 -0300

    Clean

--- README.md
+++
@@ -1,3 +0,0 @@
-# T-VVAR
-
- - https://www.figma.com/proto/U8ojEXx2vxSK2KOvoBvHVH8y/Frontend-test?node-id=13%3A42&scaling=contain
--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'vvar-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-  title = 'VVAR';
-}
