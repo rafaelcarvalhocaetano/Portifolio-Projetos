4d7438e6fadaab9b1da2ce564e4e599962f4a82d
Rafael Carvalho <rapha.pse@outlook.com>
2022-02-05 23:50:43 -0300

    clean repository

--- api-flights/src/common/models/model.ts
+++
@@ -1,3 +0,0 @@
-export const USER = { name: 'users' };
-export const PASSEGER = { name: 'passagers' };
-export const FLIGHT = { name: 'flights' };
--- api-flights/src/passenger/entities/passenger.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class Passenger extends Document {
-  @ApiProperty()
-  name: string;
-  @ApiProperty()
-  email: string;
-}
